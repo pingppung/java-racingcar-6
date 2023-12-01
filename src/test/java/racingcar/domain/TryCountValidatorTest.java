b30b0b042063b75dc70da9a9fe4be89b06be9578
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-02 01:13:00 +0900

    test(TryCountValidatorTest): 최소 시도 횟수 값에 대한 예외처리 테스트

--- src/test/java/racingcar/domain/TryCountValidatorTest.java
+++ src/test/java/racingcar/domain/TryCountValidatorTest.java
@@ -21,4 +21,20 @@ public class TryCountValidatorTest {
         String name = "2";
         TryCountValidator.validateNonNumberic(name);
     }
+
+    @DisplayName("최소 시도 횟수인 1미만의 값을 입력했을 경우 예외 발생")
+    @Test
+    void invalidateMinTryCount() {
+        int count = 0;
+        assertThatThrownBy(() -> TryCountValidator.validateMinTryCount(count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 1이상 입력되어야 레이싱 경주가 가능합니다.");
+    }
+
+    @DisplayName("최소 시도 횟수인 1이상의 값을 입력했을 경우")
+    @Test
+    void validateMinTryCount() {
+        int count = 1;
+        TryCountValidator.validateMinTryCount(count);
+    }
 }
