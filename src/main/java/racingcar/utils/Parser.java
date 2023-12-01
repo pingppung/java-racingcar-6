ccbe25f5156d0b2589d2afdbbf972065183a27dc
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 17:32:36 +0900

    feat(Parser): 자동차 이름은 쉼표(,)를 기준으로 구분하는 기능 구현

---
+++ src/main/java/racingcar/utils/Parser.java
@@ -0,0 +1,14 @@
+package racingcar.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+    private static final String SEPARATOR = ",";
+
+    public List<String> parseCarNames(String input) {
+        return Arrays.stream(input.split(SEPARATOR))
+                .map(String::trim)
+                .toList();
+    }
+}
