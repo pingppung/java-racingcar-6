ad2d6603cd5c06f7b44d1f8f296cb011129bd99b
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 15:20:36 +0900

    feat(InputView): 몇 번의 이동을 할 것인지 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_CARS_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_PROMPT = "시도할 회수는 몇회인가요?";
 
     public String getCarNames() {
         return inputWithPrompt(INPUT_CARS_PROMPT);
+    }
+
+    public String getTryCount() {
+        return inputWithPrompt(INPUT_TRY_PROMPT);
     }
 
     private String inputWithPrompt(String prompt) {
