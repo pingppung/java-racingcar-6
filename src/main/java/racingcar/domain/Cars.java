5a659886dac8cb5d4611b136fb5cbf0d647c391d
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 15:06:19 +0900

    feat(Cars): 각 자동차을 리스트로 관리하는 일급 컬렉션 Cars 클래스 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars() {
+        this.cars = new ArrayList<>();
+    }
+}
