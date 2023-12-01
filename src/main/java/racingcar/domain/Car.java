1be55edb4cce0ea545450d96b2c71bb7d75f0be1
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 15:01:45 +0900

    feat(Car): 각 자동차의 이름과 위치를 저장하는 Car 클래스 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,11 @@
+package racingcar.domain;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+}
