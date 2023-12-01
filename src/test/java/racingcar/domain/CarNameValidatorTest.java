f9509450bc43b9b39446d42ffad427c4855a9d94
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-02 01:04:33 +0900

    test(CarNameValidatorTest): 자동차 이름 중복에 대한 예외처리 테스트

--- src/test/java/racingcar/domain/CarNameValidatorTest.java
+++ src/test/java/racingcar/domain/CarNameValidatorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class CarNameValidatorTest {
 
-    @DisplayName("1대의 자동차 이름만 입력했을 경우")
+    @DisplayName("1대의 자동차 이름만 입력했을 경우 예외 발생")
     @Test
     void invalidateNameCount_singleCarName() {
         List<String> carNames = List.of("Car1");
@@ -17,7 +17,7 @@ public class CarNameValidatorTest {
                 .hasMessage("[ERROR] 2대 이상 입력되어야 레이싱 경주가 가능합니다");
     }
 
-    @DisplayName("2대 이상의 자동차 이름을 입력했을 경우")
+    @DisplayName("2대 이상의 자동차 이름을 입력했을 경우 정상")
     @Test
     void validateNameCount_multipleCarNames() {
         List<String> multipleCarNames = List.of("Car1", "Car2");
@@ -47,5 +47,21 @@ public class CarNameValidatorTest {
     void validateNameLengthInRange() {
         String name = "Car";
         CarNameValidator.validateNameLength(name);
+    }
+
+    @DisplayName("자동차 이름이 중복되는 경우 예외 발생")
+    @Test
+    void invalidateNameDuplicate() {
+        List<String> duplicatedNames = List.of("Car1", "Car2", "Car2");
+        assertThatThrownBy(() -> CarNameValidator.validateNameDuplicate(duplicatedNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름이 중복됩니다.");
+    }
+
+    @DisplayName("중복되는 자동차 이름이 없을 경우 정상")
+    @Test
+    void validateNameDuplicate() {
+        List<String> uniqueCarNames = List.of("Car1", "Car2", "Car3");
+        CarNameValidator.validateNameDuplicate(uniqueCarNames);
     }
 }
