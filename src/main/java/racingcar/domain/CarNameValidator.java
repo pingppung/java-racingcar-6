ec137099cc94d3e4cc0261e9956927babb3edfd0
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-01 18:11:48 +0900

    feat(CarNameValidator): 자동차 이름이 중복되는 경우 예외 처리되도록 유효성 검사 구현

--- src/main/java/racingcar/domain/CarNameValidator.java
+++ src/main/java/racingcar/domain/CarNameValidator.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarNameValidator {
     private static final int MIN_NAME_LENGTH = 1;
@@ -11,6 +13,13 @@ public class CarNameValidator {
             if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
                 throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상, 5자 이하만 가능합니다.");
             }
+        }
+    }
+
+    public void validateNameDuplicate(List<String> carNames) {
+        Set<String> names = new HashSet<>(carNames);
+        if (names.size() != carNames.size()) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름이 중복됩니다.");
         }
     }
 }
